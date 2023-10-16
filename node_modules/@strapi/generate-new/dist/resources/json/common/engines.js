bddffcdb4a17cdfa5d413043ec51d185ad3d1451
Satya Bhattarai <satyabhattarai@Shantanus-Mac-mini.local>
2023-10-16 15:47:10 +0545

    first

--- node_modules/@strapi/generate-new/dist/resources/json/common/engines.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    node: '>=14.19.1 <=18.x.x',
-    npm: '>=6.0.0',
-};
-//# sourceMappingURL=engines.js.map
